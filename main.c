a0aede61da9ad12866fdc02e8b3ffcc1b220ae7a
DanielTezolin <tezolind@gmail.com>
2022-11-23 16:25:35 +0000

    Novas adicoes

--- main.c
+++ main.c
@@ -1,21 +1,46 @@
-#include "./src/login.h"
+#include "./src/login.c"
+#include "./src/menus.h"
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
 
-void exitProgram() {
+void exitProgram(void);
+
+void mainAccess(void);
+void userAccess(void);
+
+void usersMenu(void);
+void mainMenu(void);
+
+int main(void);
+
+int main()
+{
+  setlocale(LC_ALL, "Portuguese");
+  system("clear");
+  // login();
+  mainAccess();
+
+  return (0);
+}
+
+void exitProgram()
+{
   int opc;
 
   system("clear");
   printf("Tem certeza que deseja sair?.\n\n");
   printf("[1] - SIM\n[0] - NAO.\n\n");
 
-  while (1) {
+  while (1)
+  {
     scanf("%d", &opc);
     fflush(stdin);
 
-    switch (opc) {
+    switch (opc)
+    {
     case 1:
       printf("Saindo...\n");
       exit(0);
@@ -30,34 +55,66 @@ void exitProgram() {
   }
 }
 
-int main() {
+void mainAccess()
+{
   int menu = 0;
 
-  login();
+  system("clear");
+  mainMenu();
 
-  while (1) {
-    // system("clear");
-    printf("Selecione uma opcao.\n\n");
-    printf("Cadastro - 1\nRelatorios - 2\nSair - 3\n\n");
-    scanf("%d", &menu);
-    fflush(stdin);
+  printf("\nSelecione uma opcao e pressione ENTER: ");
+  scanf("%d", &menu);
+  fflush(stdin);
 
-    switch (menu) {
-    case 1:
-      printf("Entrou no Cadastro\n");
-      break;
-    case 2:
-      printf("Entrou no Relatorios.\n");
-      break;
-    case 3:
-      printf("Entrou no Sair.\n");
-      exitProgram();
-      break;
-    default:
-      printf("Opcao invalida, tente novamente.\n");
-      break;
-    }
+  switch (menu)
+  {
+  case 1:
+    userAccess();
+    break;
+  case 2:
+    printf("Entrou no Relatorios.\n");
+    break;
+  case 3:
+    printf("Entrou no Sair.\n");
+    exitProgram();
+    break;
+  default:
+    printf("Opcao invalida, tente novamente.\n");
+    break;
   }
+}
 
-  return (0);
+void userAccess()
+{
+  int menu = 0;
+
+  system("clear");
+  /*---- Acesso ao menu de usuários ----*/
+  usersMenu();
+
+  printf("\nSelecione uma opcao e pressione ENTER: ");
+  scanf("%d", &menu);
+  fflush(stdin);
+
+  switch (menu)
+  {
+  case 1:
+    printf("Lista de Usuários .\n");
+    break;
+  case 2:
+    printf("Buscar Usuários.\n");
+    break;
+  case 3:
+    printf("Excluir Usuários.\n");
+    break;
+  case 4:
+    printf("Cadastrar Usuários .\n");
+    break;
+  case 5:
+    mainAccess();
+    break;
+  default:
+    printf("Opcao invalida, tente novamente.\n");
+    break;
+  }
 }
---
+++ src/menus.h
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/*MENU TELAS*/
+
+void mainMenu(void)
+{
+	setlocale(LC_ALL, ""); //Para acentuação
+	printf("\033[2J\033[1;1H");
+
+	printf("	      ========   Prote-g   ========\n\n");
+
+	printf("    .____________________________________________________.\n");
+	printf("    |                                                    |\n");
+	printf("    |                   Menu principal                   |\n");
+	printf("    |____________________________________________________|\n");
+	printf("    |                                                    |\n");
+	printf("    |       [1] - Menu de Usuários                       |\n");
+  printf("    |       [2] - Menu de Colaboradores                  |\n");
+  printf("    |       [3] - Menu de Clientes                       |\n");
+	printf("    |       [4] - Menu de Relatorios                     |\n");
+	printf("    |       [5] - Sair do Programa                       |\n");
+	printf("    |                                                    |\n");
+	printf("    |____________________________________________________|\n");
+}
+
+void usersMenu(void)
+{
+	setlocale(LC_ALL, ""); //Para acentuação
+	printf("\033[2J\033[1;1H");
+
+	printf("	      ========   Prote-g   ========\n\n");
+
+	printf("    .____________________________________________________.\n");
+	printf("    |                                                    |\n");
+	printf("    |                   Menu Usuários                    |\n");
+	printf("    |____________________________________________________|\n");
+	printf("    |                                                    |\n");
+	printf("    |       [1] - Lista de Usuários                      |\n");
+  printf("    |       [2] - Buscar Usuários                        |\n");
+  printf("    |       [3] - Excluir Usuários                       |\n");
+	printf("    |       [4] - Cadastrar Usuários                     |\n");
+	printf("    |       [5] - Voltar                                 |\n");
+	printf("    |                                                    |\n");
+	printf("    |____________________________________________________|\n");
+}
